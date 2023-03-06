d276fa52aaa8b4c94782dff3f63f020fc0e3621f
Harneet <rishih91@gmail.com>
2023-03-05 17:27:01 -0800

    initial content for pages 1,2

---
+++ data/1_overview.py
@@ -0,0 +1,8 @@
+# Imports
+import streamlit as st
+
+# Content
+st.set_page_config(page_title="Overwview")
+
+st.markdown("# Overview")
+st.sidebar.header("Overview")
---
+++ data/2_raw-data.py
@@ -0,0 +1,8 @@
+# Imports
+import streamlit as st
+
+# Content
+st.set_page_config(page_title="Raw Data")
+
+st.markdown("# Raw Data")
+st.sidebar.header("Raw Data")
