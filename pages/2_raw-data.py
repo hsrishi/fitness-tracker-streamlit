29239f6816bcabb8e6da4813741302abc77e6e5e
Harneet <rishih91@gmail.com>
2023-03-05 21:33:40 -0800

    add page 2 content for displaying raw data

--- pages/2_raw-data.py
+++ pages/2_raw-data.py
@@ -1,8 +1,14 @@
 # Imports
 import streamlit as st
+import pandas as pd
+
+from utils.misc import load_data
 
 # Content
 st.set_page_config(page_title="Raw Data")
 
 st.markdown("# Raw Data")
 st.sidebar.header("Raw Data")
+
+df = load_data()
+df
