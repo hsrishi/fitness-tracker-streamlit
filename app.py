42c3bf96d7881a70ad94eabe746f7c5cf57b8d7c
Harneet <rishih91@gmail.com>
2023-03-05 18:25:47 -0800

    add sidebar info

--- app.py
+++ app.py
@@ -3,7 +3,12 @@ import streamlit as st
 
 
 if __name__ == "__main__":
+    st.set_page_config(
+        page_title="Fitness Tracker",
+        page_icon="👋",
+    )
     # title/sidebar
     text = st.sidebar.title("Contents")
     title = st.title("Fitness Tracker")
     
+    st.sidebar.success("Select a section from above.")
